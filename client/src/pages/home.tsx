91aa60bd541c56f0260ef4cfa1b9b35aa82741e7
serendipity88 <42191278-serendipity88@users.noreply.replit.com>
2025-06-06 07:28:07 +0000

    Restored to '52c32028fb73ab775fa488e9e4d831ceb361acc3'
    Replit-Restored-To: 52c32028fb73ab775fa488e9e4d831ceb361acc3

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -3,6 +3,7 @@ import { useAppContext } from "@/contexts/AppContext";
 import { Header } from "@/components/layout/header";
 import { BottomNavigation } from "@/components/layout/bottom-navigation";
 import { ProductCardStack } from "@/components/product/product-card-stack";
+import { InlineFilter } from "@/components/filter/inline-filter";
 import { Lists } from "@/pages/lists";
 import { InfoPanel } from "@/components/info-panel";
 import { ShareModal } from "@/components/share-modal";
@@ -34,6 +35,9 @@ export default function Home() {
       <main className="container mx-auto px-4 pb-24 pt-4 flex-1">
         {currentView === View.EXPLORE && (
           <div className="flex flex-col items-center">
+            {/* 인라인 필터 */}
+            <InlineFilter />
+            
             {/* 상품 컨텐츠 */}
             <div className="w-full flex flex-col items-center">
               <ProductCardStack />
